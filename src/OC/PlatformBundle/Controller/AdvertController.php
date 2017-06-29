7ca4e6a18cc4f70d51e5631ed4ac5dada2c2a934
limozaza <zakboite@gmail.com>
2017-06-30 00:46:27 +0200

    My first test

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+    public function indexAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',['name' => "Boufares"]);
+        return new Response($content);
+    }
+}
