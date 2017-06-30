75ac78a565a28c36964313b57faa56b5bf78f95b
limozaza <zakboite@gmail.com>
2017-06-30 14:28:18 +0200

    Add route tuto

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -12,4 +12,17 @@ class AdvertController extends Controller
         $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig',['name' => "Boufares"]);
         return new Response($content);
     }
+
+    public function viewAction($id)
+    {
+        return new Response("L'annace d'id $id");
+    }
+
+    public function viewSlugAction($slug,$year,$format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
+    }
 }
