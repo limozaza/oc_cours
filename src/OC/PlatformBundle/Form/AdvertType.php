84bbcb020c4fc6273c5b076bfec34f8c038d0f89
limozaza <zakboite@gmail.com>
2017-07-10 18:25:31 +0200

    Add Form imbrique p01

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -28,6 +28,7 @@ class AdvertType extends AbstractType
                     'required'=>false
                 ]
             )
+            ->add('image', ImageType::class)
             ->add('save', SubmitType::class);
     }
     
